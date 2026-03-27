4240765caf26785e549d880ae98d569a3154174f
The1Krutz <the1krutz@gmail.com>
2026-03-27 12:25:14 -0400

    getting started

---
+++ PloverCore/Conditions/Bloodied.cs
@@ -0,0 +1,6 @@
+
+namespace PloverCore;
+
+public class Bloodied
+{
+}
---
+++ PloverCore/Conditions/Condition.cs
@@ -0,0 +1,9 @@
+
+namespace PloverCore;
+
+public struct Condition{
+  public int Id;
+  public string Name;
+  public string Description;
+
+}
